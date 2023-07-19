d8a29e79e0c721671c82f2171fe4b70cd506e3c7
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-07-19 20:23:36 +0900

    ObjectType & Arguments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
