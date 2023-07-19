d79d8eb17b55da47f828dbebe5fabed5a56d63fd
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-07-19 23:19:02 +0900

    ch02 Graphql Api + Nest.js (#1)
    * Apollo Server Setup + Resolver
    
    * ObjectType & Arguments
    
    * 2.4 InputTypes and ArgumentTypes
    
    * 2.5 Validating ArgsTypes

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,17 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+
+@Resolver(of => Restaurant)
+export class RestaurantResolver {
+  @Query(returns => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+
+  @Mutation(returns => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(createRestaurantDto);
+    return true;
+  }
+}
